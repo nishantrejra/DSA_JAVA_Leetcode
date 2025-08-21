d578e61a48313ff886bfb4bf3cea4a8554d5c45a
Nishant Rejra <nishantrejra094@gmail.com>
2025-08-21 15:39:18 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (30.56%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -3,12 +3,12 @@ class Solution {
         int [] arr = new int[digits.length + 1];
 
         for(int i = digits.length -1 ; i>=0; i--){
-            if(digits[i] < 9 || digits[i] == 0){
+            if(digits[i] < 9 ){
                 digits[i] += 1;
                 return digits;
-            } else if(digits[i] == 9){
+            } 
                 digits[i] = 0;
-            }
+            
         }
         int[] newArr = new int[digits.length + 1];
         newArr[0] = 1;
