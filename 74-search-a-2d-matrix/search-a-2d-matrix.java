9a07270f8873b686aa32111bcaf5cd00ee327304
Nishant Rejra <nishantrejra094@gmail.com>
2025-09-11 21:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (50.54%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,5 +1,30 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int left = 0, right = (n * m) - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midVal = matrix[mid / m][mid % m];
+
+            if (target == midVal) {
+                return true;
+            } else if (target > midVal) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+
+    }
+}
+
+/* Brute_Froce_Solution
+ class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
         for(int i = 0; i<matrix.length ; i++){
             for(int j = 0; j<matrix[0].length;j++){
                 if(matrix[i][j] == target){
@@ -9,4 +34,4 @@ class Solution {
         }
         return false;
     }
-}
+} */
