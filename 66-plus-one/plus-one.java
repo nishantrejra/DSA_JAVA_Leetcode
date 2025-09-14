84d8ef25dcdefed38535a285634af4e8c8e8c93f
Nishant Rejra <nishantrejra094@gmail.com>
2025-09-15 02:01:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (68.11%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int [] arr = new int[digits.length + 1];
 
         for(int i = digits.length -1 ; i>=0; i--){
             if(digits[i] < 9 ){
