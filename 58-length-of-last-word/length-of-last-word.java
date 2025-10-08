8b5aa23a7b203ea9a8aa9896ae2e09bd49def158
Nishant Rejra <nishantrejra094@gmail.com>
2025-10-08 10:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (96.91%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,10 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        //java1
-        s = s.trim();
-        String[] str = s.split("\\s+");
-        String word = str[str.length -1];
-        int count = word.length();
-        return count;
+        return s.trim().length() - s.trim().lastIndexOf(" ") -1;
     }
 }
+
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         //java1
+//         s = s.trim();
+//         String[] str = s.split("\\s+");
+//         String word = str[str.length -1];
+//         int count = word.length();
+//         return count;
+//     }
+// }
